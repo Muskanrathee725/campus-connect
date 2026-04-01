27e56277b0343c764ad8a4f0849612cb1cba2cc3
Muskan Rathee <muskanrathee725@gmail.com>
2026-04-01 18:26:54 +0530

    initial commit - campus connect

---
+++ campus-connect/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-50">
+      <h1 className="text-4xl font-bold text-blue-600">Campus Connect</h1>
+      <p className="text-gray-500 mt-2">Connecting CU students</p>
+    </div>
+  );
+}
